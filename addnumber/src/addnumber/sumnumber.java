20b783d57297e1f3136a81999bd548b7abe4a4b4
SameedSiddiqi <u2017314@gmail.com>
2021-09-08 13:29:22 +0500

    Commiting JAVA PROJECT

---
+++ addnumber/src/addnumber/sumnumber.java
@@ -0,0 +1,11 @@
+package addnumber;
+
+public class sumnumber {
+	 public static void main(String[] args) {
+	        
+	      int num1 = 5, num2 = 15, sum;
+	      sum = num1 + num2;
+
+	      System.out.println("Sum of these numbers: "+sum);
+	   }
+}
